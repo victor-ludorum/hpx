9c8a75225264df11a531b306b683ff5e906aec03
victor-ludorum <33842504+victor-ludorum@users.noreply.github.com>
2018-03-22 19:28:24 +0530

    Create histogram_performance_counters.hpp

---
+++ hpx/performance_counters/server/histogram_performance_counters.hpp
@@ -0,0 +1,102 @@
+//  Copyright (c) 2018 Surya Priy
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(HPX_PERFORMANCE_COUNTERS_SERVER_HISTOGRAM_PERFORMANCE_COUNTER)
+#define HPX_PERFORMANCE_COUNTERS_SERVER_HISTOGRAM_PERFORMANCE_COUNTER
+
+#include <hpx/config.hpp>
+#include <hpx/lcos/local/spinlock.hpp>
+#include <hpx/performance_counters/server/base_performance_counter.hpp>
+#include <hpx/runtime/components/server/component_base.hpp>
+#include <hpx/runtime/naming/id_type.hpp>
+#include <hpx/util/interval_timer.hpp>
+#include <hpx/util/detail/pp/nargs.hpp>
+#include <hpx/util/function.hpp>
+#include <hpx/util/histogram.hpp>
+#include <hpx/util/pool_timer.hpp>
+
+#include <boost/smart_ptr/scoped_ptr.hpp>
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <vector>
+
+#include <hpx/config/warnings_prefix.hpp>
+
+///////////////////////////////////////////////////////////////////////////////
+namespace hpx { namespace performance_counters { namespace server
+{
+    ///////////////////////////////////////////////////////////////////////////
+    // This counter exposes the histogram for counters processed during the
+    // given base time interval. The counter relies on querying a steadily
+    // growing counter value.
+    class histogram_performance_counter
+    {
+       typedef hpx::lcos::local::spinlock mutex_type;
+        
+        // avoid warnings about using this in member initializer list
+        histogram_performance_counter* this_() { return this; }
+
+    public:
+        histogram_counter() {}
+        typedef histogram_performance_counter type_holder;
+        typedef base_performance_counter base_type_holder;
+
+        std::vector<std::int64_t>
+            get_histogram(bool reset);
+        void get_histogram_creator(
+            std::int64_t min_boundary, std::int64_t max_boundary,
+            std::int64_t num_buckets,
+            util::function_nonser<std::vector<std::int64_t>(bool)>& result);
+        
+        // register the given action
+        static void register_action(char const* action, error_code& ec);
+        
+        get_histogram_values_type get_histogram_counter(
+            std::string const& name, std::int64_t min_boundary,
+            std::int64_t max_boundary, naming::gid_type& gid,
+            std::int64_t num_buckets);
+
+        /// Overloads from the base_counter base class.
+        hpx::performance_counters::counter_value
+            get_counter_value(bool reset = false);
+
+        bool start();
+
+        bool stop();
+
+        void reset_counter_value();
+
+        void on_terminate() {}
+
+        /// \brief finalize() will be called just before the instance gets
+        ///        destructed
+        void finalize()
+        {
+            base_performance_counter::finalize();
+            base_type::finalize();
+        }
+
+    private:
+        
+        typedef boost::accumulators::accumulator_set<
+                double,     // collects percentiles
+                boost::accumulators::features<hpx::util::tag::histogram>
+            > histogram_collector_type;
+
+        std::unique_ptr<histogram_collector_type> data_histogram;
+        std::int64_t histogram_min_boundary_;
+        std::int64_t histogram_max_boundary_;
+        std::int64_t histogram_num_buckets_;
+        
+        // base counters to be queried
+        performance_counter_set counters_;
+        mutable mutex_type mtx_;
+    };
+}}}
+
+#endif
