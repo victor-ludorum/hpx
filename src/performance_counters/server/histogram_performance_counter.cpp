35ca9a58aed7f6465230f6819bfbdb604d6d082a
victor-ludorum <33842504+victor-ludorum@users.noreply.github.com>
2018-03-25 02:05:26 +0530

    Update histogram_performance_counter.cpp

--- src/performance_counters/server/histogram_performance_counter.cpp
+++ src/performance_counters/server/histogram_performance_counter.cpp
@@ -14,13 +14,25 @@
 #include <hpx/util/unlock_guard.hpp>
 #include <hpx/performance_counters/counters.hpp>
 #include <hpx/performance_counters/counter_creators.hpp>
-#include <hpx/performance_counters/counters_fwd.hpp>
 #include <hpx/performance_counters/stubs/performance_counter.hpp>
-#include <hpx/performance_counters/server/histogram_performance_counter.hpp>
+#include <hpx/performance_counters/server/histogram_counter.hpp>
+
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/mean.hpp>
 
 #include <hpx/util/iterator_range.hpp>
 
 #include <boost/parameter/keyword.hpp>
+#include <boost/accumulators/framework/accumulator_base.hpp>
+#include <boost/accumulators/framework/extractor.hpp>
+#include <boost/accumulators/numeric/functional.hpp>
+#include <boost/accumulators/framework/parameters/sample.hpp>
+#include <boost/accumulators/framework/depends_on.hpp>
+#include <boost/accumulators/statistics_fwd.hpp>
+#include <boost/accumulators/statistics/count.hpp>
+#include <boost/accumulators/statistics/max.hpp>
+#include <boost/accumulators/statistics/min.hpp>
 
 #if defined(HPX_MSVC)
 #  pragma warning(push)
@@ -55,22 +67,198 @@ namespace hpx { namespace performance_counters { namespace server
     namespace detail
     {
    
+    }
+    
+     histogram_performance_counter::histogram_performance_counter(
+     counter_info const& info, std::string const& base_counter_name,
+     std::int64_t histogram_min_boundary, std::int64_t histogram_max_boundary,
+     std::int64_t histogram_num_buckets, bool reset_base_counter)
+      : base_type_holder(info),
+        timer_(util::bind(&histogram_performance_counter::evaluate, this_()),
+            util::bind(&histogram_performance_counter::on_terminate, this_()),
+            std::string(action_name) + "_timer", info.fullname_, true),
+        base_counter_name_(ensure_counter_prefix(base_counter_name)),
+        has_prev_value_(false),
+        histogram_min_boundary_(-1),
+        histogram_max_boundary_(-1),
+        histogram_num_buckets_(-1),
+        reset_base_counter_(reset_base_counter)
+    {
+       registry::instance().register_action(action_name,
+            util::bind_front(&histogram_performance_counter::
+                get_histogram_creator, this));
+            
+        if (info.type_ != counter_aggregating) {
+            HPX_THROW_EXCEPTION(bad_parameter,
+                "histogram_performance_counter::histogram_performance_counter",
+                "unexpected counter type specified");
+        }
+
+        // make sure this counter starts collecting data
+        start();
+    }
+    
+    bool histogram_performance_counter::evaluate()
+    {
+        // gather current base value
+        counter_value base_value;
+        if (!evaluate_base_counter(base_value))
+            return false;
+
+        // simply average the measured base counter values since it got queried
+        // for the last time
+        counter_value value;
+        if (base_value.scaling_ != prev_value_.scaling_ ||
+            base_value.scale_inverse_ != prev_value_.scale_inverse_)
+        {
+            // not supported right now
+            HPX_THROW_EXCEPTION(not_implemented,
+                "statistics_counter<Statistic>::evaluate",
+                "base counter should keep scaling constant over time");
+            return false;
+        }
+        else {
+            // accumulate new value
+            std::lock_guard<mutex_type> l(mtx_);
+         //   value_->add_value(static_cast<double>(base_value.value_));
+            
+            // collecting data for histogram
+            if(data_histogram)
+                (*data_histogram)(static_cast<double>(base_value.value_));
+        }
+        return true;
+    }
+
+    bool histogram_performance_counter::ensure_base_counter()
+    {
+        // lock here to avoid checking out multiple reference counted GIDs
+        // from AGAS. This
+        std::unique_lock<mutex_type> l(mtx_);
+
+        if (!base_counter_id_) {
+            // get or create the base counter
+            error_code ec(lightweight);
+            hpx::id_type base_counter_id;
+            {
+                // We need to unlock the lock here since get_counter might suspend
+                util::unlock_guard<std::unique_lock<mutex_type> > unlock(l);
+                base_counter_id = get_counter(base_counter_name_, ec);
+            }
+
+            // After reacquiring the lock, we need to check again if base_counter_id_
+            // hasn't been set yet
+            if (!base_counter_id_)
+            {
+                base_counter_id_ = base_counter_id;
+            }
+            else
+            {
+                // If it was set already by a different thread, return true.
+                return true;
+            }
+
+            if (HPX_UNLIKELY(ec || !base_counter_id_))
+            {
+                // base counter could not be retrieved
+                HPX_THROW_EXCEPTION(bad_parameter,
+                    "histogram_performance_counter::evaluate_base_counter",
+                    hpx::util::format(
+                        "could not get or create performance counter: '%s'",
+                        base_counter_name_));
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool histogram_performance_counter::evaluate_base_counter(
+        counter_value& value)
+    {
+        // query the actual value
+        if (!base_counter_id_ && !ensure_base_counter())
+            return false;
+
+        value = stubs::performance_counter::get_value(
+            launch::sync, base_counter_id_, reset_base_counter_);
+
+        if (!has_prev_value_)
+        {
+            has_prev_value_ = true;
+            prev_value_ = value;
+        }
+
+        return true;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Start and stop this counter. We dispatch the calls to the base counter
+    // and control our own interval_timer.
     bool histogram_performance_counter::start()
     {
-        return counters_.start(hpx::launch::sync);
+        if (!timer_.is_started()) {
+            // start base counter
+            if (!base_counter_id_ && !ensure_base_counter())
+                return false;
+
+            bool result = stubs::performance_counter::start(
+                launch::sync, base_counter_id_);
+            if (result) {
+                // acquire the current value of the base counter
+                counter_value base_value;
+                if (evaluate_base_counter(base_value))
+                {
+                    std::lock_guard<mutex_type> l(mtx_);
+               //   value_->add_value(static_cast<double>(base_value.value_));
+                    prev_value_ = base_value;
+                    
+                   // collecting data for histogram
+                   if(data_histogram)
+                        (*data_histogram)(static_cast<double>(base_value.value_));                    
+                }
+
+                // start timer
+                timer_.start();
+            }
+            else {
+                // start timer even if base counter does not support being
+                // start/stop operations
+                timer_.start(true);
+            }
+            return result;
+        }
+        return false;
     }
 
     bool histogram_performance_counter::stop()
     {
-        return counters_.stop(hpx::launch::sync);
+        if (timer_.is_started()) {
+            timer_.stop();
+
+            if (!base_counter_id_ && !ensure_base_counter())
+                return false;
+            return stubs::performance_counter::stop(
+                launch::sync, base_counter_id_);
+        }
+        return false;
     }
 
-    bool histogram_performance_counter::reset_counter_value()
+    void histogram_performance_counter::reset_counter_value()
     {
-        counters_.reset(hpx::launch::sync);
-    }
-        
-    }
+        std::lock_guard<mutex_type> l(mtx_);
+
+        // reset accumulator
+        // value_.reset(new detail::counter_type_from_histogram(
+        //    parameter2_));
+
+        // start off with last base value
+       //  value_->add_value(static_cast<double>(prev_value_.value_));
+
+        // collecting data for histogram
+        if(data_histogram)
+             (*data_histogram)(static_cast<double>(base_value.value_));        
+    }    
+
 }}}
 
 namespace hpx { namespace performance_counters { namespace detail
